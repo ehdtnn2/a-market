d226315e9009fcfcf4533dba54266793e96adb8f
ehdtnn <ehdtnn2@gmail.com>
2023-07-19 21:23:49 +0900

    init

---
+++ src/main/java/com/amarket/AmarketApplication.java
@@ -0,0 +1,13 @@
+package com.amarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amarket/AmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
